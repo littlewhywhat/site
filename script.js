b7fd161fe4a3b0c3c7b38ac08d8b2c467c5871ca
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 17:25:10 +0400

    script.js: added hover handler

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ var s = Snap("#main");
 Snap.load("main.svg", function(f) {
 	g = f.select("g");
 	f.selectAll("text").attr({visibility : 'hidden'});
-	
 	s.append(g);
+});
+
+$("circle").hover(function() {
+	$(this).closest("text").attr({visibility : 'display'});
 });
 
 
