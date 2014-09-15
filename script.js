6d96640f4d69edbbdff290856d87159a206b6efa
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 17:32:28 +0400

    script.js: changed

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ Snap.load("main.svg", function(f) {
 	g = f.select("g");
 	f.selectAll("text").attr({visibility : 'hidden'});
 	s.append(g);
-});
-
-$("circle").hover(function() {
-	$(this).closest("text").toggle();
+	$("circle").hover(function() {
+		$(this).closest("text").toggle();
+	});
 });
 
 
