fc4b361db0abd26d5e40571c64a0a995cb44536f
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 16:42:13 +0400

    removed unnecessary code and added main.svg 	modified:   index.html - changed #scheme to div, added svg 	modified:   script.js - removed useless code

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
 
 
-var s = Snap("#scheme");
-var circle = s.circle(500,500,100);
-circle.mouseover(function() {
-	circle.attr({
-		fill:'#f00'
-	});
-});
-circle.mouseout(function() {
-	circle.attr({
-		id: 'circle',
-		fill:'#000'
-	});
-});
-circle.drag();
-circle.dblclick(function() {
-	var duration = 200;
-	circle.animate({r: 110}, 10, function() {
-		circle.animate({r: 100}, duration)
-	});
-});
-Snap.load("mobile_cloud_services.svg", function(f) {
-	g = f.select("g");
-	s.append(g);
-});
+
 
 
 
