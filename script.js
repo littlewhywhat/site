6ff6ba057e079bea759809bb4e8e4ce62bcc8b3e
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 18:06:00 +0400

    script.js: changed hover call from Jquery to Snap

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ var s = Snap("#main");
 Snap.load("main.svg", function(f) {
 	g = f.select("g");
 	f.selectAll("text").attr({visibility : 'hidden'});
+	f.selectAll("circle").hover(
+		function() {
+			$(this).closest("text").toggle();
+		},
+		function() {
+			$(this).closest("text").toggle();
+		});
 	s.append(g);
-	$("circle").hover(function() {
-		$(this).closest("text").toggle();
-	});
+	
 });
 
 
