2beac3c99667c2d8d29b2b763409732a86b3ff22
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 17:16:31 +0400

    script.js: fixed attr change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var s = Snap("#main");
 Snap.load("main.svg", function(f) {
 	g = f.select("g");
-	f.selectAll("text").attr({visibility : hidden});
+	f.selectAll("text").attr({visibility : 'hidden'});
 	f.selectAll("circle").drag();
 	s.append(g);
 });
