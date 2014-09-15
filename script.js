0ec9e4b05f84ec39a0d2ffeea4a48def72a2e1d1
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 18:16:15 +0400

    script.js:added call to foreach

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ var s = Snap("#main");
 Snap.load("main.svg", function(f) {
 	g = f.select("g");
 	f.selectAll("text").attr({visibility : 'hidden'});
-	f.selectAll("circle").hover(
+	f.selectAll("circle").foreach(function(element) {
+		element.hover(
 		function() {
 			$(this).closest("text").toggle();
 		},
 		function() {
 			$(this).closest("text").toggle();
 		});
+	});
 	s.append(g);
 	
 });
