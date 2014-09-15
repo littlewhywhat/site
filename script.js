06739744be593ab7af1759f3ebd35490d7386c28
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 17:09:53 +0400

    script.js: hided texts and made circles dragable

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var s = Snap("#main");
 Snap.load("main.svg", function(f) {
 	g = f.select("g");
+	f.selectAll("text").attr({visibility : hidden});
+	f.selectAll("circle").drag();
 	s.append(g);
 });
 
