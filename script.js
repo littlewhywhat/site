6f916f4c8d4c42e05cdefa13c3b29389c80eb3e8
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 19:13:39 +0400

    script.js: made something working with circles

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ var s = Snap("#main");
 Snap.load("main.svg", function(f) {
 	g = f.select("g");
 	f.selectAll("text").attr({visibility : 'hidden'});
-	var circles = f.selectAll("circle");
-	for (circle in circles) {
-		circle.hover(
-		function() {
-			$(this).closest("text").toggle();
-		},
-		function() {
-			$(this).closest("text").toggle();
-		});
-	};
 	s.append(g);
 	
+	var circles = s.selectAll("ellipse");
+	circles.attr({'pointer-events':'auto'});
+	circles.forEach(function(circle) {		
+		circle.hover(function() {
+			circle.attr({fill : 'violet'});
+		},
+		function() {
+			circle.attr({fill : 'white'});
+		});
+	},
+	circles);
 });
+
+
 
 
 
