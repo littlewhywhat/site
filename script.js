42bc0de7100bfa1a24849d31cd9205ae0a505f22
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-15 17:18:13 +0400

    script.js: removed drag call

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var s = Snap("#main");
 Snap.load("main.svg", function(f) {
 	g = f.select("g");
 	f.selectAll("text").attr({visibility : 'hidden'});
-	f.selectAll("circle").drag();
+	
 	s.append(g);
 });
 
