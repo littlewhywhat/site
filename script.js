5c8475492bd08455d51b282b90002e1a913246d9
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-17 01:23:07 +0400

    removed scaling, added opacity 	modified:   map.svg : added content groups 	modified:   script.js : removed initNodes,Element, added 	scale,opacity,attachHandlers

--- script.js
+++ script.js
@@ -5,47 +5,40 @@ var s = Snap(snapName);
 Snap.load(loadUrl, function(f) {
 	
 	g = f.select(mainParentTag);
+	
 	s.append(g);
-
-	var gElement = new Element(g);
-	gElement.hover(
-		function() {
-			gElement.scale(1.2, 400);
-		},
-		function() {
-			gElement.scale(1.0, 400);
-		});
-	initNodes(s);
+	
+	
+	attachHandlers(g.selectAll('.content'))
 });
+
+function attachHandlers(elements) {
+
+	elements.forEach(function(element) {
+		opacity(element, 0.25, 400);
+		element.hover(function() {
+			opacity(element, 2.0, 600);
+		}, function() {
+			opacity(element, 0.25, 600);
+		});
+	});
+}
 
 function Element(element) {
 	this.svgEl = element;
-	this.bbox = this.svgEl.getBBox();
-	this.scale = function (scale, duration) {
-		var translateX = (1 - scale) * this.bbox.cx;
-		var translateY = (1 - scale) * this.bbox.cy;
-		element.animate({transform : "scale("+ scale +") " +
-				"translate( " + translateX + ", " + translateY + ")" }, duration)
-	}
 	this.hover = function(fin, fout) {
 		element.hover(fin, fout);
 	}
 }
 
-function initNodes(s) {
-	var nodes = s.selectAll(".node");
-	nodes.forEach(function(node) {	
-		var ellipse = node.select("ellipse");	
-		var text = node.select("text");
-		ellipse.hover(
-			function() {
-				ellipse.attr({fill : 'violet'});
-				text.attr({visibility : 'visible'});
-			},
-			function() {
-				ellipse.attr({fill : 'white'});
-				text.attr({visibility : 'hidden'});
-		});
-	},
-	nodes);
+function opacity(element, opacity, duration) {
+	element.animate({'opacity': opacity}, duration)
+}
+
+function scale(element, scale, duration) {
+		var bbox = element.getBBox();
+		var translateX = (1 - scale) * bbox.cx;
+		var translateY = (1 - scale) * bbox.cy;
+		element.animate({transform : "scale("+ scale +") " +
+				"translate( " + translateX + ", " + translateY + ")" }, duration);
 }
