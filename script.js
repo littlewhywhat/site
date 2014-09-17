c27640520e74e486fc9bf737855a4f2774955638
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-17 14:27:09 +0400

    added moving animation, removed unused code 	modified:   map.svg : removed translate in g 	modified:   script.js : changed g to global mainParent, scale, added 	recordScalePosition, click in attachHandlers, removed Element 	modified:   stylesheet.css : added cursor:pointer in #main

--- script.js
+++ script.js
@@ -2,19 +2,30 @@ var snapName = "#main";
 var mainParentTag = "g";
 var loadUrl = "map.svg";
 var s = Snap(snapName);
+var mainParent,
+	scalePositionX,
+	scalePositionY;
 Snap.load(loadUrl, function(f) {
 	
-	g = f.select(mainParentTag);
+	mainParent = f.select(mainParentTag);
 	
-	s.append(g);
+	s.append(mainParent);
 	
-	
-	attachHandlers(g.selectAll('.content'))
+	recordScalePosition(mainParent);
+	attachHandlers(mainParent.selectAll('.content'))
 });
 
-function attachHandlers(elements) {
+function recordScalePosition(mainParent) {
+	var bbox = mainParent.getBBox();
+	scalePositionX = bbox.w / 4;
+	scalePositionY = bbox.h / 2;
+}
 
+function attachHandlers(elements) {
 	elements.forEach(function(element) {
+		element.click( function() {
+			scale(element, 1.2, 1000);
+		}, element);
 		opacity(element, 0.25, 400);
 		element.hover(function() {
 			opacity(element, 2.0, 600);
@@ -24,21 +35,19 @@ function attachHandlers(elements) {
 	});
 }
 
-function Element(element) {
-	this.svgEl = element;
-	this.hover = function(fin, fout) {
-		element.hover(fin, fout);
-	}
-}
-
 function opacity(element, opacity, duration) {
 	element.animate({'opacity': opacity}, duration)
 }
 
 function scale(element, scale, duration) {
-		var bbox = element.getBBox();
-		var translateX = (1 - scale) * bbox.cx;
-		var translateY = (1 - scale) * bbox.cy;
-		element.animate({transform : "scale("+ scale +") " +
-				"translate( " + translateX + ", " + translateY + ")" }, duration);
+	var bbox = element.getBBox();
+	var x = bbox.cx;
+	var y = bbox.cy;
+	var translateX =  (scalePositionX - x);
+	var translateY =  (scalePositionY - y);
+	var matrix = new Snap.Matrix();
+	matrix.translate(translateX, translateY);
+	mainParent.animate({
+		transform: matrix.toTransformString()
+	}, duration, mina.bounce);
 }
