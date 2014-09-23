38ac6b118e4bb328d07c57ecf420c9581c9ccdb8
Roman Vayvod <littlewhywhat@gmail.com>
2014-09-23 21:32:37 +0400

    added license to js files 	modified:   script.js 	modified:   src/eventstracker.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Copyright 2014 Roman Vayvod
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 $( document ).on( "mobileinit", function() {
   $.mobile.loader.prototype.options.text = "";
 });
@@ -137,7 +152,7 @@ function Popup(site) {
 		instance.animHide();
 		site.unfocus();
 	}
-	
+
 	this.isReady = function() {
 		return !$element.is(':animated');
 	}
